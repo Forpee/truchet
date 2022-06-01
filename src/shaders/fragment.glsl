01603a57d293aae9eae59b0b921664f1d6d4587a
Forpee <khalilhassam3@gmail.com>
2022-06-01 19:30:22 +0200

    update

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -13,5 +13,20 @@ void main()
     
     vec2 nUv=6.*vec2(8.*angle,vUv.y*5.);
     vec2 gUv=fract(nUv)-.5;
-    gl_FragColor=vec4(vUv,1.,1.);
+    
+    vec2 id=mod(floor(nUv),vec2(6.));
+    float n=hash(id);
+    gUv.x*=2.*step(.5,n)-1.;
+    
+    float d=abs(abs(gUv.x+gUv.y)-.5);
+    
+    float mask=smoothstep(-.01,.01,d-.3);
+    
+    if(mask<.0001)discard;
+    
+    gl_FragColor=vec4(vec3(mask),mask);
+    
+    if(!gl_FrontFacing){
+        gl_FragColor.a*=.5;
+    };
 }
