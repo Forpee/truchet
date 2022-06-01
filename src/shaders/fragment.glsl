d1a082a3bad4459a61445059a334b1c8e1c898a5
Forpee <khalilhassam3@gmail.com>
2022-06-01 19:26:46 +0200

    update

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,8 +1,17 @@
 uniform float uTime;
 
+varying vec3 vPosition;
 varying vec2 vUv;
+
+float PI=3.1415926535897932384626433832795;
+
+float hash(vec2 p){return fract(1e4*sin(17.*p.x+p.y*.1)*(.1+abs(sin(p.y*13.+p.x))));}
 
 void main()
 {
-    gl_FragColor = vec4(vUv, 1.0, 1.0);
+    float angle=(atan(vPosition.y,vPosition.x)+PI)/(2.*PI);
+    
+    vec2 nUv=6.*vec2(8.*angle,vUv.y*5.);
+    vec2 gUv=fract(nUv)-.5;
+    gl_FragColor=vec4(vUv,1.,1.);
 }
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,10 +1,12 @@
 varying vec2 vUv;
+varying vec3 vPosition;
 
 void main()
 {
     // vec4 mvPosition=modelViewMatrix*vec4(position,1.);
     // gl_PointSize=100.*(1./-mvPosition.z);
     vUv=uv;
+    vPosition=position;
     
     vec3 newPos=position;
     
